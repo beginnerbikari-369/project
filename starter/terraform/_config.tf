c190f96df85edd90a2ffe09c4f0a83341ac126ec
CHANDRAMOULI7981 <66708996+beginnerbikari-369@users.noreply.github.com>
2022-06-04 21:24:09 +0530

    Add files via upload

---
+++ starter/terraform/_config.tf
@@ -0,0 +1,15 @@
+terraform {
+   backend "s3" {
+     bucket = "awsnewtf"
+     key    = "terraform/terraform.tfstate"
+     region = "us-east-1"
+   }
+ }
+
+ provider "aws" {
+   region = "us-east-1"
+
+   default_tags {
+     tags = local.tags
+   }
+ }
